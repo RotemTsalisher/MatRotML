c0548d278ae365bf94076dc7ba6ee4786c323689
DESKTOP-T9T2COA\420 <rtm.tsalisher@gmail.com>
2024-01-28 11:48:55 +0200

    28.1 DONE

--- HW01/FINAL/HW1Q2_Matan_Porat_Rotem_Tsalisher.py
+++ HW01/FINAL/HW1Q2_Matan_Porat_Rotem_Tsalisher.py
@@ -12,7 +12,17 @@
 # * Cells with 'TO BE COMPLETED' require you to complete the code according to the given description 
 # * **NOTE**: After you complete the notebook and run all cells, **discuss** the results displayed in the graph produced at the end of the notebook 
 
-# In[212]:
+# In[30]:
+
+
+# block dedicated to import needed libraries
+import numpy as np
+from numpy.random import randint
+import matplotlib.pyplot as plt
+from scipy.special import comb
+
+
+# In[31]:
 
 
 # replace the strings below with the personal details of the two students submitting this notebook
@@ -22,10 +32,8 @@ id1 = '318734944' # student 1 id
 id2 = '203773601' # student 2 id
 
 
-# In[213]:
+# In[32]:
 
-
-import numpy as np
 
 def LoadIrisData(fname):
     '''
@@ -46,10 +54,9 @@ def LoadIrisData(fname):
     return X, Y
 
 
-# In[214]:
+# In[33]:
 
 
-from numpy.random import randint
 def SplitTrainTest(X,Y):
     '''
     ***COMPLETED - NO CHANGES ARE REQUIRED***
@@ -74,10 +81,19 @@ def SplitTrainTest(X,Y):
     return X_train, Y_train, X_test, Y_test
 
 
-# In[215]:
+# In[34]:
 
 
 def findAxisVal(x):
+    
+    ###################################################################
+    # a function to determine the neede value for 'axis' 
+    # attribute of the {later used} function "linalg.norm"
+    #
+    # shape returns (n,m) dimentions of matrix given by x
+    # if matrix collaps into a 1D vector, the len(shape) will return 1
+    ###################################################################
+    
     condition = len(x.shape)
     if(condition == 1):
         axis_ = 0
@@ -86,6 +102,7 @@ def findAxisVal(x):
     return axis_
 
 
+# In[35]:
 
 
 def PairwiseDistance(a,b):
@@ -105,7 +122,7 @@ def PairwiseDistance(a,b):
     return np.linalg.norm(a-b,axis = axis_)
 
 
-# In[216]:
+# In[36]:
 
 
 def SortArray(a):
@@ -123,7 +140,7 @@ def SortArray(a):
     return np.argsort(a)
 
 
-# In[217]:
+# In[37]:
 
 
 def MajorityVote(inds,y,K):
@@ -161,7 +178,7 @@ def MajorityVote(inds,y,K):
     return unique[index]
 
 
-# In[218]:
+# In[38]:
 
 
 def KNearestNeighborsClassifier(X_train, Y_train , X_test, K):
@@ -195,10 +212,8 @@ def KNearestNeighborsClassifier(X_train, Y_train , X_test, K):
     return Y_pred
 
 
-# In[219]:
+# In[39]:
 
-
-import matplotlib.pyplot as plt
 
 def PlotAccuracy(accuracy):
 
@@ -210,10 +225,11 @@ def PlotAccuracy(accuracy):
     plt.xlabel('K')
     plt.ylabel('Accuracy')
     plt.title('KNN estimated accuracy for various values of K');
+    plt.grid() # it bothers my eye not to see the grid 
     return
 
 
-# In[220]:
+# In[40]:
 
 
 def Accuracy(Y_pred, Y_test):
@@ -239,7 +255,7 @@ def Accuracy(Y_pred, Y_test):
     return ((np.sum(Y_pred==Y_test))/len(Y_test))
 
 
-# In[221]:
+# In[41]:
 
 
 def main(fname, Kmax):
@@ -265,12 +281,11 @@ def main(fname, Kmax):
         accuracy[K] = Accuracy(Y_pred, Y_test)
         
     # plot results
-    print(accuracy)
     PlotAccuracy(accuracy)
     return
 
 
-# In[222]:
+# In[42]:
 
 
 fname = 'iris.csv'
@@ -280,7 +295,7 @@ main(fname, Kmax)
 
 # # Second Part Of Q2:
 
-# In[223]:
+# In[43]:
 
 
 def SplitTrainTestMatRot(X,Y):
@@ -291,7 +306,8 @@ def SplitTrainTestMatRot(X,Y):
     ind = np.random.RandomState(seed=int(id1)+int(id2)).permutation(len(Y))
 
     # randomly choose the size of the training data
-    Ntrain = 80 + np.random.randint(31)
+    Ntrain = 80 + np.random.randint(31) # <= a correction to choose an actual random number within inclusive range [80,110]
+    # (otherwise, function will always return 92 for Ntrain, based on section 1 rand mechanism)
 
     # split the data into train and test datasets
     X_train = X[ind[:Ntrain]]
@@ -302,18 +318,20 @@ def SplitTrainTestMatRot(X,Y):
     return X_train, Y_train, X_test, Y_test
 
 
-# In[244]:
+# In[46]:
 
 
 def oneHundredMiles(Kmax):
     
-    G = 3 # number of itterations
+    G = 100 # number of itterations
     
     # STEP 1: load data
     X,Y = LoadIrisData(fname)
     
-    # an array to store all computed accuracies
+    # a matrix to store all computed accuracies
     accuracyMat = np.zeros((G,Kmax))
+    
+    # a vector to store mean values for given k in the previous matrix
     accuracyMean = np.zeros(Kmax)
     for i in range(G):
         
@@ -331,15 +349,79 @@ def oneHundredMiles(Kmax):
             accuracyMat[i,K] = Accuracy(Y_pred, Y_test)   
     
     
-    accuracyMean = np.mean(accuracyMat,axis = 0)
+    accuracyMean = np.mean(accuracyMat,axis = 0) #averaging over column sapce
     PlotAccuracy(accuracyMean)
     return
 
 
-# In[245]:
+# In[47]:
 
 
-oneHundredMiles(100)
+oneHundredMiles(100) # sometimes functions runtime get's a little slow, be patient
+
+
+# # Question 3
+
+# According to manual Calculations, the p that will get us maximun likelyhood for the sieres (H/T) given in the question
+# is p = K/N.
+
+# In[53]:
+
+
+def calcProb(p,N,K):
+    
+    binomial_coeff = comb(N, K) 
+    pmf_ = binomial_coeff*(p**K) * ((1 - p)**(N - K))
+    
+    return pmf_
+
+
+# In[54]:
+
+
+def maxProb(N,K):
+    p_ml = K/N # applying manually calculated formula
+    pmf_p = calcProb(p_ml,N,K)
+    plt.plot(p_ml, pmf_p, marker="o", markeredgecolor="red", markerfacecolor="green") # adding the point to the probability plot
+    return
+
+
+# In[59]:
+
+
+def plotProbFunc(pmf_,p_axis,N,K): 
+    plt.figure()
+    plt.plot(p_axis,pmf_)
+    maxProb(N,K)
+    plt.xlabel("0 < p < 1")
+    plt.ylabel(" f(p) - pmf of binomial variable ")
+    title = "Probability Mass Function for N = " + str(N) + ", K = " + str(K)
+    plt.title(title);
+    plt.grid()
+
+    return
+    
+
+
+# In[63]:
+
+
+def Q3():
+    T = 1000 # points of evaluation
+    p_axis = np.linspace(0,1,T)
+    pmf1 = calcProb(p_axis,10,3)
+    pmf2 = calcProb(p_axis,50,15)
+    pmf3 = calcProb(p_axis,300,90)
+    plotProbFunc(pmf1,p_axis,10,3)
+    plotProbFunc(pmf2,p_axis,50,15)
+    plotProbFunc(pmf3,p_axis,300,90)
+    return
+
+
+# In[64]:
+
+
+Q3() # activate
 
 
 # In[ ]:
